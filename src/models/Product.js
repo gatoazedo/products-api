514d911b9758e128bdcd5aa68b940a7de94a9adb
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-10 03:14:02 -0300

    Registered products now have timestamp

--- src/models/Product.js
+++ src/models/Product.js
@@ -9,9 +9,9 @@ const ProductSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
+  createdAt: {
+    type: Date,
+    default: Date.now
   }
 })
 
