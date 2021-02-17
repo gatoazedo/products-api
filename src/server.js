b9e1b5b97f3d22806a7408c9b1ebb93904c4c8b5
Guilherme Azevedo <guilhermeazevedo.dev@gmail.com>
2021-02-17 15:45:31 -0300

    Cors implemented

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 mongoose.connect(
   'mongodb+srv://guilherme:k20.c20xe-ap@products-api.znpil.mongodb.net/products-api?retryWrites=true&w=majority',
